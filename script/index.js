61ad554936ee29fc2db0fbbdb55bece930582a17
Edson0012 <edsonewertonrs1@gmail.com>
2022-07-02 01:50:19 -0300

    terminando a tela 1 e os requisitos da listagem de todos os quizzes

--- script/index.js
+++ script/index.js
@@ -1,15 +1,9 @@
 let infoQuizz;
-let api = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+let quizzes;
+const API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
-function obterQuizz() {
-    const promise = axios.get(api);
-
-    promise.then((resposta) => mensagens(resposta));
-    promise.catch(mensagens);
-}
-
-function mensagens(mensagem) {
-    infoQuizz = mensagem.data;
+function recarregarPagina() {
+    window.location.reload();
 }
 
 function criarQuizz() {
@@ -17,9 +11,28 @@ function criarQuizz() {
     const adicionarquizz = document.querySelector(".tela-3-1");
     criarquizz.classList.add("escondido");
     adicionarquizz.classList.remove("escondido");
-    promise = axios.post(api, infoQuizz);
 }
 
-function recarregarPagina() {
-    window.location.reload();
+function pegarQuizzes() {
+    const promise = axios.get(API);
+    promise.then((resposta) => {
+        quizzes = resposta.data;
+        listquizzes();
+    });
+}
+
+pegarQuizzes();
+
+function listquizzes() {
+    let box = document.querySelector(".caixas-quizz");
+    box.innerHTML = "";
+    for (let i = 0; i < quizzes.length; i++) {
+        let dataQuizz = quizzes[i];
+        box.innerHTML += ` <div class="box-quiz" id="${dataQuizz.id}">  
+            <img  width="340px" height="181px" src="${dataQuizz.image}" />
+            <div class="sombra">
+            <span class="title">${dataQuizz.title}</span>
+            </div>
+        </div>`;
+    }
 }
